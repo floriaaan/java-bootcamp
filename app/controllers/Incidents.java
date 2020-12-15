077dedd805b8b6a3019f05a3249185aa01c666df
Florian Leroux <florian.leroux3@laposte.net>
2020-12-15 18:39:51 +0100

    feat(style): incident show

--- app/controllers/Incidents.java
+++ app/controllers/Incidents.java
@@ -24,6 +24,7 @@ public class Incidents extends Rights {
      */
     public static void show(Long id) {
         Incident incident = Incident.findById(id);
+        // TODO : render mission as mission
         render(incident);
     }
 
