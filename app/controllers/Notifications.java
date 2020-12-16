7b386af44c8ab11d5640978e6729db04b17e6f09
Florian Leroux <florian.leroux3@laposte.net>
2020-12-16 17:22:53 +0100

    feat(notification): set to read

--- app/controllers/Notifications.java
+++ app/controllers/Notifications.java
@@ -18,7 +18,15 @@ public class Notifications extends Rights {
      */
     public static void showAll() {
         Citizen auth = getAuth();
-        List<Notification> nList = Notification.find("citizen.id", auth.id).fetch();
-        render(nList);
+        List<Notification> read = Notification.find("citizen.id = ?1 and is_viewed = 1", auth.id).fetch();
+
+
+        List<Notification> unread = Notification.find("citizen.id = ?1 and is_viewed = 0", auth.id).fetch();
+
+        for (Notification n : unread) {
+            n.is_viewed = true;
+            n.save();
+        }
+        render(unread, read);
     }
 }
