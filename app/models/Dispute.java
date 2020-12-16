3b639f4d508bfae134f2a2a8617afbe61aae54a4
Ophelie ZEITEL <ophelie.zeitel@viacesi.fr>
2020-12-16 10:25:20 +0100

    [ADD] Dispute model/controller + add link in Mission model

---
+++ app/models/Dispute.java
@@ -0,0 +1,43 @@
+package models;
+
+import play.db.jpa.Model;
+import play.data.validation.*;
+import io.ebean.*;
+import java.util.*;
+import javax.persistence.*;
+import play.data.format.*;
+
+import play.db.ebean.*;
+import com.avaje.ebean.*;
+
+@Entity(name="disputes")
+public class Dispute extends Model {
+
+    @Required
+    public String title;
+
+    @OneToOne
+    public Mission mission;
+
+    @OneToOne
+    public Citizen citizen;
+
+    public String comments;
+
+    @Required
+    public float amount;
+
+    public Date created_at;
+    public Date updated_at;
+
+    @PrePersist
+    public void created_at() {
+        this.created_at = new java.util.Date();
+        this.updated_at = new java.util.Date();
+    }
+
+    @PreUpdate
+    public void updated_at() {
+        this.updated_at = new java.util.Date();
+    }
+}
