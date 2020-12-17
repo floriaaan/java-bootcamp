f44582d1142ae2f4172ce5eea471bf1daaa05824
GODARD Anatole <anatole.godard@icloud.com>
2020-12-17 01:08:16 +0100

    add validation on the super hero

--- app/controllers/SuperController.java
+++ app/controllers/SuperController.java
@@ -5,6 +5,7 @@ import play.mvc.*;
 import play.mvc.Scope.*;
 
 import java.util.*;
+
 import play.data.validation.*;
 
 import models.*;
@@ -14,43 +15,40 @@ import models.*;
  */
 public class SuperController extends Controller {
 
-    public SuperController()
-    {
+    public SuperController() {
         super();
     }
 
     private static Citizen INSTANCE;
 
-    public static Citizen getAuth()
-    {
+    public static Citizen getAuth() {
         return INSTANCE;
     }
 
-    public static void setAuth(Citizen c)
-    {
+    public static void setAuth(Citizen c) {
         INSTANCE = c;
     }
 
 
-    @Before(unless={"Authentication.login", "Authentication.connect", "Authentication.register", "Authentication.logout", "Citizens.create"})
+    @Before(unless = {"Authentication.login", "Authentication.connect", "Authentication.register", "Authentication.logout", "Citizens.create"})
     public void middleware_auth() {
 
-        if(this.INSTANCE == null) {
+        if (this.INSTANCE == null) {
             //System.out.println("unauth");
             flash.error("Please log in.");
             params.flash();
             Validation.keep();
             renderTemplate("Authentication/login.html");
         } else {
-            //System.out.println(this.INSTANCE.getName());
             long userSuperheroIdentity = SuperHero.count("identity = ?1", this.INSTANCE);
             long notifications = Notification.count("citizen = ?1 AND is_viewed = 0", this.INSTANCE);
+            long isUserSuperhero = SuperHero.count("identity = ?1 AND is_validate = true", this.INSTANCE);
 
 
             renderArgs.put("user", this.INSTANCE);
             renderArgs.put("superhero", userSuperheroIdentity);
             renderArgs.put("notifications", notifications);
+            renderArgs.put("validatesuperhero", isUserSuperhero);
         }
     }
-
 }
